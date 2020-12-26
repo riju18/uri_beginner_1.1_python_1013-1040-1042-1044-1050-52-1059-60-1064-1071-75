abf54ad355aae74c42bfb7fc19ac28266030046a
riju18 <rijuchowdhury18@gmail.com>
2020-12-26 20:24:08 +0600

    1044 is accepted

--- 1044_multiples.py
+++ 1044_multiples.py
@@ -1,7 +1,10 @@
-numbers = str(input(''))
-split = numbers.split()
-a, b = list(map(int, split))
-if b % a == 0:
-    print('Sao Multiplos\n')
-else:
-    print('Nao sao Multiplos\n')
+numbers = str( input( '' ) )
+split_numbers = numbers.split()
+a = list( map( int, split_numbers ) )[:2]
+x = a[0]
+y = a[1]
+if x != 0:
+    if y % x == 0 or x % y == 0:
+        print( 'Sao Multiplos' )
+    else:
+        print( 'Nao sao Multiplos' )
