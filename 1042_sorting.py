0761f5ec7cd1232cd8339a0022962b1f77cabc8b
riju18 <rijuchowdhury18@gmail.com>
2020-12-26 19:53:36 +0600

    1042 presentation error is solved

--- 1042_sorting.py
+++ 1042_sorting.py
@@ -1,9 +1,10 @@
-numbers = str(input(''))
-split = numbers.split()
-a = list(map(int, split))
-sortt = sorted(a)
-for i in sortt:
-    print(i)
-print('\n')
+numbers = str( input( '' ) )
+split_numbers = numbers.split()
+a = list( map( int, split_numbers ) )
+a = a[:3]
+sort_numbers = sorted( a )
+for i in sort_numbers:
+    print( i )
+print( '' )
 for i in a:
-    print(i)
+    print( i )
--- 1073_evenSqr.py
+++ 1073_evenSqr.py
@@ -1,9 +1,10 @@
 number = int(input('enter a number: '))
 allEven = []
 
-for i in range(1, number + 1):
-    if i % 2 == 0:
-        allEven.append(i)
-
+if number >= 5 and number <= 2000:
+       for i in range(1, number + 1):
+                if i % 2 == 0:
+                        allEven.append(i)
+                        
 for i in allEven:
     print(i,'^','2',' = ',i**2)
